6d16ee9d7131ed6fca44098269b8ce9f4d00743c
Klee Thomas <klee.ut@gmail.com>
2022-10-17 15:35:07 +1100

    :alembic: simple return

--- functions/api/wordForDay.ts
+++ functions/api/wordForDay.ts
@@ -1,8 +1,13 @@
-import { proxyAPI } from "../../server";
-export const onRequestGet: PagesFunction = async ({
-  request,
-}: {
-  request: Request;
-}) => {
-  return await proxyAPI({ request });
+// import { proxyAPI } from "../../server";
+export const onRequestGet: PagesFunction = async ({ request }) => {
+  return new Response(
+    JSON.stringify({
+      id: 474,
+      solution: "marsh",
+      print_date: "2022-10-05",
+      days_since_launch: 473,
+    }),
+    { headers: { "content-type": "application/json" } }
+  );
+  // return await proxyAPI({ request });
 };
